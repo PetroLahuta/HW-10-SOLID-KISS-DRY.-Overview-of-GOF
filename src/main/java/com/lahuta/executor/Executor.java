d95849979513d4154b340d22429fc7f85e0e72b5
Петя <Lagutap93@gmail.com>
2020-07-23 20:35:16 +0300

    Fix: change public name in the Gitar class to privat name; Fix: show that two objects of gitar are equal; Fix: use all classes in 'Executor'

--- src/main/java/com/lahuta/executor/Executor.java
+++ src/main/java/com/lahuta/executor/Executor.java
@@ -1,9 +1,7 @@
 package main.java.com.lahuta.executor;
 
 import main.java.com.lahuta.builder.Clerk;
-import main.java.com.lahuta.builder.proceduralDoc.FirstDoc;
-import main.java.com.lahuta.builder.proceduralDoc.ProcedularDocsBuilder;
-import main.java.com.lahuta.builder.proceduralDoc.ProceduralDoc;
+import main.java.com.lahuta.builder.proceduralDoc.*;
 import main.java.com.lahuta.singleton.Gitar;
 
 public class Executor {
@@ -14,18 +12,52 @@ public class Executor {
 
     private static void startBuildDoc() {
         Clerk clerk = new Clerk();
+
         ProcedularDocsBuilder firstDoc = new FirstDoc();
         clerk.setProcedularDocsBuilder(firstDoc);
         clerk.setShowNewProceduralDoc();
+        ProceduralDoc proceduralDocOne = clerk.getProceduralDoc();
+        System.out.println(proceduralDocOne);
 
-        ProceduralDoc proceduralDoc = clerk.getProceduralDoc();
-        System.out.println(proceduralDoc);
+        ProcedularDocsBuilder secondDoc = new SecondDoc();
+        clerk.setProcedularDocsBuilder(secondDoc);
+        clerk.setShowNewProceduralDoc();
+        ProceduralDoc proceduralDocTwo = clerk.getProceduralDoc();
+        System.out.println(proceduralDocTwo);
+
+        ProcedularDocsBuilder thirdDoc = new ThirdDoc();
+        clerk.setProcedularDocsBuilder(thirdDoc);
+        clerk.setShowNewProceduralDoc();
+        ProceduralDoc proceduralDocThree = clerk.getProceduralDoc();
+        System.out.println(proceduralDocThree);
+
+        ProcedularDocsBuilder fourthDoc = new FourthDoc();
+        clerk.setProcedularDocsBuilder(fourthDoc);
+        clerk.setShowNewProceduralDoc();
+        ProceduralDoc proceduralDocFour = clerk.getProceduralDoc();
+        System.out.println(proceduralDocFour);
+
+        ProcedularDocsBuilder fiveDoc = new FifthDoc();
+        clerk.setProcedularDocsBuilder(fiveDoc);
+        clerk.setShowNewProceduralDoc();
+        ProceduralDoc proceduralDocFive = clerk.getProceduralDoc();
+        System.out.println(proceduralDocFive);
+
+        ProcedularDocsBuilder sixDoc = new SixthDoc();
+        clerk.setProcedularDocsBuilder(sixDoc);
+        clerk.setShowNewProceduralDoc();
+        ProceduralDoc proceduralDocSix = clerk.getProceduralDoc();
+        System.out.println(proceduralDocSix);
     }
 
     private static void startSingletonGitar() {
         Gitar.getInstance("Gibson Les Paul");
-        System.out.println("Gitar: " + Gitar.name);
+        System.out.println("Gitar: " + Gitar.getName());
         Gitar.getInstance("Trembita");
-        System.out.println("Gitar: " + Gitar.name);
+        System.out.println("Gitar: " + Gitar.getName());
+
+        Gitar gitar = Gitar.getInstance(Gitar.getName());
+        Gitar gitar2 = Gitar.getInstance(Gitar.getName());
+        System.out.println(gitar.equals(gitar2));
     }
 }
--- src/main/java/com/lahuta/singleton/Gitar.java
+++ src/main/java/com/lahuta/singleton/Gitar.java
@@ -2,13 +2,17 @@ package main.java.com.lahuta.singleton;
 
 public class Gitar {
     private static Gitar instance;
-    public static String name;
+    private static String name;
+
+    public static String getName() {
+        return name;
+    }
 
     private Gitar(String name) {
         this.name = name;
     }
 
-    public static Gitar getInstance(String name){
+    public static Gitar getInstance(String name) {
         if (instance == null) {
             instance = new Gitar(name);
         }
